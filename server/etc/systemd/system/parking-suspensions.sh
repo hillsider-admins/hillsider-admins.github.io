3c77c18320270bcef291da3ebfaf52f24a31881d
Stephen Taylor <stephen@kx.com>
2021-08-20 16:48:15 +0100

    removed duplicate parking suspensions

--- server/etc/systemd/system/parking-suspensions.sh
+++ server/etc/systemd/system/parking-suspensions.sh
@@ -1,42 +1,32 @@
 #!/bin/bash
 # title: Retrieve parking suspensions as HTML pages from camden.gov.uk
 # author: Stephen Taylor
-# date: 11 Jun 2020
+# date: 20  Aug 2021
+# DRY: Don't Repeat Yourself
 # usage: run as root
 
 url="http://registers.camden.gov.uk/SuspendedBays/Suspensions.aspx"
 dest="/var/www/hillsiders.org/data"
 
 srcs=('NASSINGTON%20ROAD' 'PARLIAMENT%20HILL' 'SOUTH%20HILL%20PARK' 'TANZA%20ROAD')
-tgts=(nass phill south tanza)
 
 lst=$((${#srcs[*]} - 1))
 
-rm -f "$dest/*"
+rm -f $dest/table.html
 for i in `seq 0 $lst`
 do
     echo ${srcs[$i]}
-    tgt="$dest/${tgts[$i]}.html"
-    curl -s "$url?street=${srcs[$i]}" > $tgt
+    src="$url?street=${srcs[$i]}"
+    curl -s $src | grep -h '<t[rd]>' >> $dest/table.html
     if [ $? -eq 0 ]
     then
-        echo  "Created $tgt"
+        echo  "Copied from $src"
         continue
     else
-        echo "Could not write $tgt" >&2
+        echo "Could not write table file" >&2
         exit 1
     fi
 done
 
-rm $dest/table.html
-grep -h '<t[rd]>' $dest/*.html > $dest/table.html
-
-if [ $? -eq 0 ]
-then
-    echo All done
-else
-    echo "Failed to grep to $dest/table.html" >&2
-    exit 2
-fi
-
+echo "All done"
 exit 0
